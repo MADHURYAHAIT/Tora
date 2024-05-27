163d6a2ae731b7a0f1357ba357fff776145e8034
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-05-27 12:51:09 +0530

    first init

---
+++ app/_layout.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+const Rootlayout = () => {
+  return (
+    <Slot/>
+    
+  )
+}
+
+export default Rootlayout
+
+const styles = StyleSheet.create({
+    container: {
+        display:'flex',
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "react-native-web";
+export default function Page() {
+  return (
+    <View className="flex-1 items-center justify-center bg-primary">
+
+        <Text className="color-white">Hello ! World</Text>
+        <StatusBar style="auto"/>
+        <Link href={"/profile"}>Go To Profile</Link>
+
+    </View>
+  );
+}
+
