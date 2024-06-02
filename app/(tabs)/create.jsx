f757269622cd00d2be7568060ed317c0eba5dbbe
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-06-02 22:35:45 +0530

    file uploader working

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,11 +1,83 @@
-import { View, Text, ScrollView, TouchableOpacity,Image } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity,Image,Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '../../components/FormField'
 import { icons } from '../../constants'
 import { Video,ResizeMode } from 'expo-av'
 import CustomButton from '../../components/CustomButton'
+import * as DocumentPicker from "expo-document-picker";
+import { createVideo } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
+import { Link,router } from 'expo-router'
+
 const Create = () => {
+  const {user} =useGlobalContext();
+  const openPicker = async (selectType) => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type:selectType === "image"
+          ? ["image/png", "image/jpeg"]
+          : ["video/mp4", "video/gif"]
+    });
+    if (!result.canceled) {
+      if (selectType === "image") {
+        setForm({
+          ...form,
+          thumbnail: result.assets[0],
+        });
+      }
+
+      if (selectType === "video") {
+        setForm({
+          ...form,
+          video: result.assets[0],
+        });
+      }
+    } 
+    // else {
+    //   setTimeout(() => {
+    //     Alert.alert("Document picked", JSON.stringify(result, null, 2));
+    //   }, 100);
+    // }
+  };
+
+
+
+  const submit= async()=>{
+
+    if (
+      (form.prompt === "") |
+      (form.title === "") |
+      !form.thumbnail |
+      !form.video
+    ) {
+      return Alert.alert("Please provide all fields");
+    } 
+
+    setUploading(true);
+    
+    try {
+      await createVideo({
+        ...form,
+        userId: user.$id,
+      });
+
+      Alert.alert("Success", "Post uploaded successfully");
+      router.push("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setForm({
+        title: "",
+        video: null,
+        thumbnail: null,
+        prompt: "",
+      });
+
+      setUploading(false);
+    }
+
+  }
+  
   const[uploading,setUploading]=useState(false)
   const[form,setForm]=useState({
     title:'',
@@ -15,8 +87,8 @@ const Create = () => {
   })
   return (
    <SafeAreaView className='bg-primary h-full '>
-    <ScrollView className='px-4 my-6'>
-      <Text className='text-2xl text-white font-psemibold'>
+    <ScrollView className='px-4 my-6 pt-2'>
+      <Text className='text-3xl text-white font-psemibold'>
         Upload Video
       </Text>
 
@@ -29,17 +101,17 @@ const Create = () => {
       />
 
       <View>
-        <Text className="text-base text-gray-100 font-pmedium my-3 pt-3">
+        <Text className="text-base text-gray-100 font-pmedium my-6 pt-3 ">
           Upload Video
         </Text>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => openPicker("video")}>
         {form.video ? (
               <Video
                 source={{ uri: form.video.uri }}
                 className="w-full h-64 rounded-2xl"
-                useNativeControls
+
                 resizeMode={ResizeMode.COVER}
-                isLooping
+
               />
             ) : (
               <View className="w-full h-40 px-4 bg-black-100 rounded-2xl border border-black-200 flex justify-center items-center">
@@ -61,7 +133,7 @@ const Create = () => {
             Thumbnail Image
           </Text>
 
-          <TouchableOpacity onPress={() => openPicker("image")}>
+          <TouchableOpacity onPress={() => openPicker('image')}>
             {form.thumbnail ? (
               <Image
                 source={{ uri: form.thumbnail.uri }}
@@ -92,7 +164,12 @@ const Create = () => {
           otherStyles="mt-7"
         />
 
-     
+        <CustomButton
+          title="Submit & Publish"
+          handlePress={submit}
+          containerStyles="mt-7 mb-3"
+          isLoading={uploading}
+        />
 
 
     </ScrollView>
