05619555adbced1c45b72d2c18a611c8e81354aa
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-06-14 21:32:03 +0530

    .

--- app/(pages)/settings.jsx
+++ app/(pages)/settings.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { router, useLocalSearchParams } from "expo-router";
-import { View, Text, FlatList, ScrollView, TouchableOpacity, Image } from "react-native";
+import { View, Text, FlatList, ScrollView, TouchableOpacity, Image, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import useAppwrite from "../../lib/useAppwrite";
-import { searchPosts, signOut } from "../../lib/appwrite";
+
+import { searchPosts, signOut, updateUsername, updateAvatar, uploadFile, getFilePreview } from "../../lib/appwrite";
 import  SearchInput from "../../components/SearchInput";
 import EmptyState from "../../components/EmptyState";
 import   VideoCard  from "../../components/VideoCard";
@@ -17,8 +18,12 @@ import * as ImagePicker from 'expo-image-picker'
 const Bookmark = () => {
   const { query } = useLocalSearchParams();
   const { user, setUser, setIsLogged } = useGlobalContext();
-  const openPicker = async (selectType) => {
+  const [form, setForm] = useState({
+    name: '',
+    avatar: null,
+  });
 
+  const openPicker = async (selectType) => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: selectType==='image'?
       ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
@@ -32,24 +37,47 @@ const Bookmark = () => {
           avatar: result.assets[0],
         });
       }
-
     } 
   }
 
+  const updateUserName = async () => {
+    try {
+      const updatedUser = await updateUsername(form.name);
+      if (updatedUser) {
+        setUser(updatedUser);
+        Alert.alert('Username updated successfully!');
+      } else {
+        Alert.alert('Error', 'Failed to update username');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const logout = async () => {
-      await signOut();
-      setUser(null);
-      setIsLogged(false);
-  
-      router.replace("/sign-in");
-    };
-    
-    const[form,setForm]=useState({
-      name:'',
-      avatar:null,
-    })
-    
+  const updateAvatar = async () => {
+    try {
+      const uploadedAvatar = await uploadFile(form.avatar, 'image');
+      const avatarUrl = await getFilePreview(uploadedAvatar.$id, 'image');
+      
+      const updatedUser = await updateAvatar(avatarUrl);
+      if (updatedUser) {
+        setUser(updatedUser);
+        Alert.alert('Avatar updated successfully!');
+      } else {
+        Alert.alert('Error', 'Failed to update avatar');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLogged(false);
+    router.replace("/sign-in");
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView className="flex my-6 px-1">
@@ -59,60 +87,61 @@ const Bookmark = () => {
           </Text>
         </View>
         <View  className="flex px-3">
+          <FormField
+            title="User Name"
+            value={form.name}
+            placeholder="Change your existing user name..."
+            handleChangeText={(e)=>setForm({...form,name:e})}
+            otherStyles='mt-10'
+          />
+          <CustomButton
+            title={'Update changes'}
+            handlePress={updateUserName}
+            containerStyles='mt-6'
+          />
+          <View className="mt-7 space-y-2">
+            <Text className="text-base text-gray-100 font-pmedium">
+              Avatar Image
+            </Text>
 
-        <FormField
-          title="User Name"
-          value={form.name}
-          placeholder="Change your existing user name..."
-          handleChangeText={(e)=>setForm({...form,title:e})}
-          otherStyles='mt-10'
+            <TouchableOpacity onPress={() => openPicker('image')}>
+              {form.avatar ? (
+                <Image
+                  source={{ uri: form.avatar.uri }}
+                  resizeMode="cover"
+                  className="w-full h-64 rounded-2xl"
+                />
+              ) : (
+                <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 flex justify-center items-center flex-row space-x-2">
+                  <Image
+                    source={icons.upload}
+                    resizeMode="contain"
+                    alt="upload"
+                    className="w-5 h-5"
+                  />
+                  <Text className="text-sm text-gray-100 font-pmedium">
+                    Choose a file
+                  </Text>
+                </View>
+              )}
+            </TouchableOpacity>
+          </View>
+          <CustomButton
+            title="Update Changes"
+            handlePress={updateAvatar}
+            containerStyles="mt-8 mb-3"
           />
 
-          <View className="mt-7 space-y-2">
-                    <Text className="text-base text-gray-100 font-pmedium">
-                      Avatar Image
-                    </Text>
-
-                    <TouchableOpacity onPress={() => openPicker('image')}>
-                      {form.thumbnail ? (
-                        <Image
-                          source={{ uri: form.avatar.uri }}
-                          resizeMode="cover"
-                          className="w-full h-64 rounded-2xl"
-                        />
-                      ) : (
-                        <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 flex justify-center items-center flex-row space-x-2">
-                          <Image
-                            source={icons.upload}
-                            resizeMode="contain"
-                            alt="upload"
-                            className="w-5 h-5"
-                          />
-                          <Text className="text-sm text-gray-100 font-pmedium">
-                            Choose a file
-                          </Text>
-                        </View>
-                      )}
-                    </TouchableOpacity>
-                  </View>
-          <CustomButton
-          title="Update Changes"
-          handlePress={null}
-          containerStyles="mt-8 mb-3"
-          // isLoading={uploading}
-        />
-
           <Text className="text-base mt-20  text-gray-100 font-pmedium">
-              Come Back later ?           
+            Come Back later ?           
           </Text>
 
           <CustomButton
-              title={'Logout'}
-              handlePress={logout}
-              containerStyles='mt-6'
-              
-              />
-      </View>
+            title={'Logout'}
+            handlePress={logout}
+            containerStyles='mt-6'
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
